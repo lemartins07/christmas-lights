7d3395926948d37a701a59a4e0cafa8076483ed5
Leandro Martins <leandro.martins.85@gmail.com>
2022-06-16 13:59:34 -0300

    create a start and stop buttons

--- js/christmas-light.js
+++ js/christmas-light.js
@@ -2,6 +2,12 @@ export default class ChristmasLight {
   constructor(selector, intervalTime) {
     this.circleLights = document.querySelectorAll(selector);
     this.intervalTime = intervalTime || 3000;
+
+    this.btnStart = document.querySelector('.btn.start');
+    this.btnStop = document.querySelector('.btn.stop');
+
+    this.bindEvents();
+    this.addBtnEvents();
   }
 
   addEvenIntensity() {
@@ -24,17 +30,51 @@ export default class ChristmasLight {
     });
   }
 
-  changeOddIntensity() {
-    this.circleLights.forEach((item, index) => {
-      if (index % 2 !== 0) {
-        item.classList.add('active');
-        console.log(item, 'impar');
-      }
+  removeLightsActive() {
+    this.circleLights.forEach((item) => {
+      item.classList.remove('active');
+      item.classList.add('off');
+    });
+  }
+
+  removeLightsOff() {
+    this.circleLights.forEach((item) => {
+      item.classList.remove('off');
     });
   }
 
   startLights() {
-    setInterval(() => { this.addEvenIntensity('add'); }, this.intervalTime);
-    setInterval(() => { this.removeEvenIntensity('remove'); }, this.intervalTime * 2);
+    if (!this.evenLightsOn) {
+      this.addEvenIntensity();
+      this.evenLightsOn = setInterval(() => {
+        this.addEvenIntensity();
+      }, this.intervalTime);
+
+      this.removeLightsOff();
+
+      this.oddLightsOn = setInterval(() => {
+        this.removeEvenIntensity();
+      }, this.intervalTime * 2);
+    }
+  }
+
+  stopLights() {
+    clearInterval(this.evenLightsOn);
+    clearInterval(this.oddLightsOn);
+    this.evenLightsOn = null;
+    this.oddLightsOn = null;
+    this.removeLightsActive();
+  }
+
+  addBtnEvents() {
+    this.btnStart.addEventListener('click', this.startLights);
+    this.btnStop.addEventListener('click', this.stopLights);
+  }
+
+  bindEvents() {
+    this.addEvenIntensity = this.addEvenIntensity.bind(this);
+    this.removeEvenIntensity = this.removeEvenIntensity.bind(this);
+    this.stopLights = this.stopLights.bind(this);
+    this.startLights = this.startLights.bind(this);
   }
 }
